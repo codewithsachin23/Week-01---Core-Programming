2c21f846dd57864ac3b05ba2fad75e28ea679935
sachin shukla <sachinshukla232003@gmail.com>
2025-01-09 20:12:00 +0530

    Added Herry Age Calculator from Level 1

---
+++ Java Programming Elements practice Level 1/HerryAgeCalculator.java
@@ -0,0 +1,13 @@
+public class HerryAgeCalculator {
+    public static void main(String[] args) {
+        // Declare and Initialize the birth year and current year of Harry
+        int birthYear = 2000;
+        int currentYear = 2024;
+
+        // Calculate Harry's age by subtracting the birth year from the current year
+        int age = currentYear - birthYear;
+
+        // Display Harry's age in the current year
+        System.out.println("Harry's age in 2024 is " + age);
+    }
+}
